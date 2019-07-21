6c0351a2ac568f32ff1cb58f4f33bd08e1a997ca
bouzuya <m@bouzuya.net>
2019-07-21 14:01:46 +0900

    initial commit

---
+++ app/src/main/java/net/bouzuya/numberpickers/HomeFragment.kt
@@ -0,0 +1,29 @@
+package net.bouzuya.numberpickers
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import net.bouzuya.numberpickers.databinding.HomeFragmentBinding
+
+
+class HomeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+
+    private val viewModel: HomeViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return HomeFragmentBinding.inflate(inflater, container, false).also { binding ->
+            binding.lifecycleOwner = this
+            binding.viewModel = viewModel
+        }.root
+    }
+}
---
+++ app/src/main/java/net/bouzuya/numberpickers/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package net.bouzuya.numberpickers
+
+import androidx.lifecycle.ViewModel;
+
+class HomeViewModel : ViewModel() {
+    val name = "home"
+}
